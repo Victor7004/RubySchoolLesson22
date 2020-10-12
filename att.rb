e90ebb6750a7acf60946b1afcb95f61eeda51876
Victor7004 <68998629+Victor7004@users.noreply.github.com>
2020-10-12 20:05:40 +0300

    Update att.rb

--- att.rb
+++ att.rb
@@ -1,4 +1,4 @@
 #require 'sqlite3'
 db = SQLite3::Database.new 'test sqlite'
-db.execute "INSERT INTO Cars (Name , Price) VALUES ('BMW', 7777 )"
+db.execute "INSERT INTO Cars (Name , Price) VALUES ('BMW', 777 )"
 db.close 
