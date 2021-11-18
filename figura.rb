b2ee6026872393ccae4089bb55cbf63ac772e8d7
Victor7004 <viktoraleksejcuk@gmail.com>
2021-11-18 19:09:07 +0200

    Create figura.rb

---
+++ figura.rb
@@ -0,0 +1,12 @@
+a = gets.to_i
+if a == 3
+    puts "Треугольник"
+elsif a == 4
+    puts "Четырехугольник"
+elsif a == 5
+    puts "Пятиугольник"
+elsif a == 6
+    puts "Шестиугольник"
+else
+    puts "Ошибка"
+end
