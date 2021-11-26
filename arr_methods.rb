06cdbe1050d25c192e9733e3260de47f7d5c0378
Victor7004 <viktoraleksejcuk@gmail.com>
2021-11-26 15:59:30 +0200

    Create arr_methods.rb

---
+++ arr_methods.rb
@@ -0,0 +1,15 @@
+arr2 = arr1.uniq!
+arr3 =arr2.sort
+puts arr3[3]
+puts arr3.length() 
+puts arr3.include?(7)
+puts arr3.max
+print arr3
+
+#Sample Output:
+
+5
+8
+true
+11
+[1, 3, 4, 5, 6, 7, 8, 11]
