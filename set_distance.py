21416e71a04c56b5ad195e12b4f3689d341a1075
Victor7004 <viktoraleksejcuk@gmail.com>
2022-01-14 18:00:27 +0200

    Create set_distance.py

---
+++ set_distance.py
@@ -0,0 +1,12 @@
+# Напишите класс Star, имеющий переменную экземпляра distance. Создайте методы get_distance и set_distance соответственно для доступа к переменной и для ее изменения.
+class Star
+  def initialize
+    @distance
+  end
+  def get_distance
+    @distance
+  end
+  def set_distance=(distance)
+    @distance = distance
+  end
+end
