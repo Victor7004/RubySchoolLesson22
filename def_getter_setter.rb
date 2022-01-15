3c483c8f072a0cc5d565356c432c14b06103bce9
Victor7004 <viktoraleksejcuk@gmail.com>
2022-01-15 18:01:31 +0200

    Create def_getter_setter.rb

---
+++ def_getter_setter.rb
@@ -0,0 +1,10 @@
+# Напишите класс Star, имеющий переменную экземпляра distance. Создайте методы getter и setter по имени переменной, соответственно для доступа к переменной и для ее изменения.
+class Star
+    def distance
+        @distance
+    end
+    
+    def distance=(distance)
+        @distance = distance
+    end
+end
