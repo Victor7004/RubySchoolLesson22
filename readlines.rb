7866bb8452ede4f39978a88843333cf1d880482d
Victor7004 <viktoraleksejcuk@gmail.com>
2022-03-08 13:16:12 +0200

    Create readlines.rb

---
+++ readlines.rb
@@ -0,0 +1,12 @@
+# Мы также можем читать содержимое файла построчно, используя метод readlines.
+# Например:
+
+File.open("test.txt", "a+") {
+  |file| file.puts("a line of text")
+  file.puts("another line of text")
+}
+
+File.readlines("test.txt").each {
+  |line| puts " --- #{line}"
+}
+# Метод readlines считывает весь файл по одной строке и возвращает эти строки в массиве.
